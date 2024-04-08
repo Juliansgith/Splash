2a0258967f0c58455f2dd07f056aa7878aca52c3
HunterRvB <114484205+HunterRvB@users.noreply.github.com>
2024-04-08 16:35:01 +0200

    Stellingkaart component klaar en getest

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { jwtDecode as jwt_decode } from 'jwt-decode';
-import QuestionnaireList from './components/QuestionnaireList';
-import CreateQuestionnaire from './components/CreateQuestionnaire';
-import QuestionnaireDetail from './components/QuestionnaireDetail'; 
-import LoginRegisterPopup from './components/LoginRegisterPopup';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { jwtDecode as jwt_decode } from "jwt-decode";
+import QuestionnaireList from "./components/QuestionnaireList";
+import CreateQuestionnaire from "./components/CreateQuestionnaire";
+import QuestionnaireDetail from "./components/QuestionnaireDetail";
+import LoginRegisterPopup from "./components/LoginRegisterPopup";
+import StellingKaart from "./components/Stellingbox";
 
 function App() {
   const [userRole, setUserRole] = useState(null);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       const decoded = jwt_decode(token);
       setUserRole(decoded.role);
@@ -24,13 +25,28 @@ function App() {
         <nav>
           <LoginRegisterPopup setUserRole={setUserRole} />
           <Link to="/">Home</Link>
-          {userRole === 'admin' && <span> | <Link to="/create">Create New Questionnaire</Link></span>} 
+          {userRole === "admin" && (
+            <span>
+              {" "}
+              | <Link to="/create">Create New Questionnaire</Link>
+            </span>
+          )}
+          <Link to="/stellingkaart">Stelling Component</Link>
         </nav>
         <Routes>
           <Route path="/" element={<QuestionnaireList />} />
-          {userRole === 'admin' && <Route path="/create" element={<CreateQuestionnaire />} />} 
+          {userRole === "admin" && (
+            <Route path="/create" element={<CreateQuestionnaire />} />
+          )}
           <Route path="/questionnaire/:id" element={<QuestionnaireDetail />} />
+          <Route path="/stellingkaart" element={<StellingKaart />} />
         </Routes>
+        <StellingKaart
+          title="Mensen vanaf 65 jaar moeten gratis met trein, tram en bus kunnen reizen."
+          tag="Gezondheid"
+          company="Albert heijn"
+          imgcompany="assets/AHcompany.svg"
+        />
       </div>
     </Router>
   );
---
+++ client/src/components/Stellingbox.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./css/Stellingbox.css";
+
+function StellingKaart(props) {
+  const goToGoogle = () => {
+    window.location.href = "https://www.google.com";
+  };
+
+  return (
+    <div className="Stellingcontainer">
+      <h1 className="Title">{props.title}</h1>
+      <h2 className="Tag">{props.tag}</h2>
+      <div className="CompanyRedirect">
+        <div className="Company">
+          <img src={props.imgcompany} className="Imgcompany" />
+          <h2 className="Companytxt">{props.company}</h2>
+        </div>
+        <img
+          src="assets/Arrowright.svg"
+          className="Arrowimg"
+          onClick={goToGoogle}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default StellingKaart;
