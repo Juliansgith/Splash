bc7f6712a6db7bcf943da31af7c4f5eac6881e3d
HunterRvB <114484205+HunterRvB@users.noreply.github.com>
2024-04-08 17:17:29 +0200

    Fonts volgens design toegevoegd

--- client/src/App.js
+++ client/src/App.js
@@ -44,7 +44,7 @@ function App() {
         <StellingKaart
           title="Mensen vanaf 65 jaar moeten gratis met trein, tram en bus kunnen reizen."
           tag="Gezondheid"
-          company="Albert heijn"
+          company="Albert Heijn"
           imgcompany="assets/AHcompany.svg"
         />
       </div>
