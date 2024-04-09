233a33a18ffe2b4df68263bd3d97b5e3bc987c4d
Eline <94545465+eHarbrecht@users.noreply.github.com>
2024-04-09 16:35:03 +0200

    Verplaatsen stelling code robin

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { jwtDecode as jwt_decode } from "jwt-decode";
 import NavBar from './components/NavBar'; 
 import QuestionnaireList from './components/QuestionnaireList';
@@ -9,7 +9,7 @@ import QuestionnaireDetail from './components/QuestionnaireDetail';
 import LoginRegisterPopup from './components/LoginRegisterPopup';
 import Answers from './components/Answers';
 import Account from './components/accounts';
-import StellingKaart from "./components/Stellingbox";
+import QuestionnaireOverview from "./components/QuestionnaireOverview";
 
 function App() {
   const [userRole, setUserRole] = useState(null);
@@ -25,39 +25,13 @@ function App() {
   return (
     <Router>
       <div className="App">
-//         <h1>Questionnaire Application</h1>
-//         <nav>
-//           <LoginRegisterPopup setUserRole={setUserRole} />
-//           <Link to="/">Home</Link>
-//           {userRole === "admin" && (
-//             <span>
-//               {" "}
-//               | <Link to="/create">Create New Questionnaire</Link>
-//             </span>
-//           )}
-//           <Link to="/stellingkaart">Stelling Component</Link>
-//         </nav>
-//         <Routes>
-//           <Route path="/" element={<QuestionnaireList />} />
-//           {userRole === "admin" && (
-//             <Route path="/create" element={<CreateQuestionnaire />} />
-//           )}
-//           <Route path="/questionnaire/:id" element={<QuestionnaireDetail />} />
-//           <Route path="/stellingkaart" element={<StellingKaart />} />
-//         </Routes>
-//         <StellingKaart
-//           title="Mensen vanaf 65 jaar moeten gratis met trein, tram en bus kunnen reizen."
-//           tag="Gezondheid"
-//           company="Albert Heijn"
-//           imgcompany="assets/AHcompany.svg"
-//         />
         <header className="header">
           <h1>Splash</h1>
           <NavBar userRole={userRole} />
         </header>
         <div className="main-content">
         <Routes>
-          <Route path="/" element={<QuestionnaireList />} />
+          <Route path="/" element={<QuestionnaireOverview />} />
           <Route path="/answers" element={<Answers />} />
           <Route path="/account" element={<Account />} />
           {userRole === 'admin' && <Route path="/create" element={<CreateQuestionnaire />} />}
