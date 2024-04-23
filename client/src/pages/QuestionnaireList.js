e0ffe9e2eaa3eb4f78235def3ef3a1ebc79906f7
HunterRvB <114484205+HunterRvB@users.noreply.github.com>
2024-04-23 09:55:02 +0200

    question.length indicator toegevoegd

--- client/src/pages/QuestionnaireList.js
+++ client/src/pages/QuestionnaireList.js
@@ -7,6 +7,7 @@ import "../css/QuestionnaireList.css"; // Ensure this file contains the above st
 function QuestionnaireList() {
   const navigate = useNavigate();
   const [questionnaires, setQuestionnaires] = useState([]);
+
   const token = localStorage.getItem("token");
   const userId = token ? jwt_decode(token).userId : null;
 
@@ -23,24 +24,34 @@ function QuestionnaireList() {
     <div className="questionnaire-container">
       <h1 className="title">Open Questionnaires</h1>
       <div className="questionnaire-list">
-        {questionnaires.map((questionnaire) => (
+        {questionnaires.map((questionnaire, index) => (
           <div key={questionnaire._id} className="questionnaire-card">
             <div className="Stellingcontainer">
               <h2 className="Title">{questionnaire.title}</h2>
-              <h3 className="Tag">questionnaire.tag</h3>
+              <h3 className="Tag">{questionnaire.tag}</h3>
               <div className="CompanyRedirect">
                 <div className="Company">
-                  <img src="assets/AHcompany.svg" className="Imgcompany" />
-                  <h3 className="Companytxt">questionnaire.company</h3>
+                  <img
+                    src="assets/AHcompany.svg"
+                    className="Imgcompany"
+                    alt="Company Logo"
+                  />
+                  <h3 className="Companytxt">{questionnaire.company}</h3>
                 </div>
                 <img
                   src="assets/Arrowright.svg"
                   className="Arrowimg"
+                  alt="Navigate to Questionnaire"
                   onClick={() =>
                     navigate(`/questionnaire/${questionnaire._id}`)
                   }
-                ></img>
+                />
               </div>
+            </div>
+            <div className="carousel-nav">
+              <span>
+                {index + 1} of {questionnaires.length}
+              </span>
             </div>
           </div>
         ))}
