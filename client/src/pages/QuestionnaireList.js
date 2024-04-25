753fdb71617d5cc437136c0c4058df3aa813c026
HunterRvB <114484205+HunterRvB@users.noreply.github.com>
2024-04-25 11:34:39 +0200

    Navigational dots added to QuestionnaireList

--- client/src/pages/QuestionnaireList.js
+++ client/src/pages/QuestionnaireList.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { jwtDecode as jwt_decode } from "jwt-decode";
-import "../css/QuestionnaireList.css"; // Ensure this file contains the above styles
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "../css/QuestionnaireList.css";
+import { Pagination } from "swiper/modules";
 
 function QuestionnaireList() {
   const navigate = useNavigate();
@@ -16,47 +20,59 @@ function QuestionnaireList() {
       .get(`http://localhost:5000/all?userId=${userId}`)
       .then((response) => {
         setQuestionnaires(response.data);
+        console.log(questionnaires);
       })
       .catch((error) => console.log(error));
   }, []);
 
   return (
-    <div className="questionnaire-container">
-      <h1 className="title">Open Questionnaires</h1>
-      <div className="questionnaire-list">
-        {questionnaires.map((questionnaire, index) => (
-          <div key={questionnaire._id} className="questionnaire-card">
-            <div className="Stellingcontainer">
-              <h2 className="Title">{questionnaire.title}</h2>
-              <h3 className="Tag">{questionnaire.tag}</h3>
-              <div className="CompanyRedirect">
-                <div className="Company">
-                  <img
-                    src="assets/AHcompany.svg"
-                    className="Imgcompany"
-                    alt="Company Logo"
+    <>
+      <div className="questionnaire-container">
+        <h1 className="title">Open Questionnaires</h1>
+        <div className="questionnaire-list">
+          <Swiper
+            slidesPerView={1.2}
+            spaceBetween={25}
+            centeredSlides={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            {questionnaires.map((questionnaire, index) => (
+              <SwiperSlide>
+                <div key={questionnaire._id} className="Stellingcontainer">
+                  <div
+                    className={questionnaire.tag === "" && "test2" + "test"}
                   />
-                  <h3 className="Companytxt">{questionnaire.company}</h3>
+                  <h2 className="Title">{questionnaire.title}</h2>
+                  <h3 className="Tag">{questionnaire.tag}</h3>
+                  <div className="CompanyRedirect">
+                    <div className="Company">
+                      <img
+                        src="assets/AHcompany.svg"
+                        className="Imgcompany"
+                        alt="Company Logo"
+                      />
+                      <h3 className="Companytxt">{questionnaire.company}</h3>
+                    </div>
+                    <img
+                      src="assets/Arrowright.svg"
+                      className="Arrowimg"
+                      alt="Navigate to Questionnaire"
+                      onClick={() =>
+                        navigate(`/questionnaire/${questionnaire._id}`)
+                      }
+                    />
+                  </div>
                 </div>
-                <img
-                  src="assets/Arrowright.svg"
-                  className="Arrowimg"
-                  alt="Navigate to Questionnaire"
-                  onClick={() =>
-                    navigate(`/questionnaire/${questionnaire._id}`)
-                  }
-                />
-              </div>
-            </div>
-            <div className="carousel-nav">
-              <span>
-                {index + 1} of {questionnaires.length}
-              </span>
-            </div>
-          </div>
-        ))}
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
