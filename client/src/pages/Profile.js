1821412a2ff3cec45eafc952f24fd44cc8e6008f
Eline <94545465+eHarbrecht@users.noreply.github.com>
2024-05-14 10:44:47 +0200

    Name change

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { jwtDecode as jwt_decode } from 'jwt-decode';
+import NavBar from '../components/NavBar'; 
 
 function Account() {
   const [userDetails, setUserDetails] = useState({});
@@ -27,11 +28,19 @@ function Account() {
   }, [userId]);
 
   return (
-    <div>
+    <>
+      <header className="top-container">
+        <div className="logo-container">
+          <h2 className="bold">Splash</h2>
+        </div>
+      </header>
+
       <h2>Account Details</h2>
       <p>Email: {userDetails.email}</p>
       <p>Points: {userDetails.points}</p>
-    </div>
+
+      <NavBar />
+    </>
   );
 }
 
