a70aceb09912216c2b82891a01cd8a0a7a56512d
Julian Cooper <115541810+Juliansgith@users.noreply.github.com>
2024-06-26 09:28:52 +0200

    ye

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Splash.app',
+  appName: 'Splash',
+  webDir: 'build'
+};
+
+export default config;
